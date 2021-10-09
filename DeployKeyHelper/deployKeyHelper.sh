955af91e27d466f656fbb6cb75f44969ed34d2be
Mark Poepping <mark.poepping@admiralcloud.com>
2021-10-09 08:18:11 +0200

    Update deployKeyHelper.sh
    + use new key mechanism (ed25519)
    + some improvements regarding user input and file/folder exists checks to prevent errors in console

--- DeployKeyHelper/deployKeyHelper.sh
+++ DeployKeyHelper/deployKeyHelper.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-### SETUP a config.txt file with
+CONFIG=~/.ssh/config
+
+### SETUP an optional config.txt file with
 #CONFIG=~/.ssh/config // path to SSH config for this user
 #PATH_TO_REPOS=/PATH TO REPOS e.g. /opt or /home ...
 #GIT_USER=
 
-source config.txt
+if test -f "config.txt"; then
+  source config.txt
+fi
+
+read -e -i "$GIT_USER" -p "Github User: " input
+GIT_USER="${input:-$GIT_USER}"
+
+read -e -i "$PATH_TO_REPOS" -p "Path to repos (e.g./opt): " input
+PATH_TO_REPOS="${input:-$PATH_TO_REPOS}"
 
 read -p 'Repo Name: ' REPO_NAME
 
@@ -13,7 +23,7 @@ cd ~/.ssh
 
 echo "Creating SSH Key for "$REPO_NAME
 FILE=$REPO_NAME'_dk'
-ssh-keygen -t rsa -N "" -b 4096 -C $REPO_NAME -f $FILE
+ssh-keygen -t ed25519 -N "" -C $REPO_NAME -f $FILE
 
 chmod 400 $FILE
 
@@ -25,10 +35,16 @@ echo "User git" >> $CONFIG
 echo "IdentityFile ~/.ssh/$FILE" >> $CONFIG
 echo "IdentitiesOnly yes" >> $CONFIG
 
-echo "Updating Remote URL for GIT"
-cd $PATH_TO_REPOS/$REPO_NAME
-git remote set-url origin git@$REPO_NAME.github.com:$GIT_USER/$REPO_NAME
+if [ -d "$PATH_TO_REPOS/$REPO_NAME" ]; then
+  echo "Updating Remote URL for GIT"
+  cd $PATH_TO_REPOS/$REPO_NAME
+  git remote set-url origin git@$REPO_NAME.github.com:$GIT_USER/$REPO_NAME
+else
+  echo "Run the following script after you have cloned git repo and you discover connection issues"
+  echo "git remote set-url origin git@$REPO_NAME.github.com:$GIT_USER/$REPO_NAME"
+fi
 
+echo ""
 echo "Copy this as deployment key"
 echo ""
 cat ~/.ssh/$FILE'.pub'
