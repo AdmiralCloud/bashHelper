f04b545d2c927a23d6297da9d88fe91191a63c87
Mark Poepping <mark.poepping@admiralcloud.com>
2023-05-12 17:13:26 +0200

    Global function for "Sourcing credentials with an external process"

--- awsConnect/getRoleSession.sh
+++ awsConnect/getRoleSession.sh
@@ -1,38 +1,44 @@
-#
-# use a cron job to fetch credentials for an IAM role
-# call this script with argument of the role
+#!/bin/bash
+
+# This script must be provided at a global location (e.g. /opt/scripts) on non-EC2 instances.
+# 
+
 # ./getRoleSession ROLENAME PROFILE
 # e.g. ./getRoleSession.sh ac-stat-instance-role dev
+# The use the script in ~/.aws/config for a given profile to retrieve and manage credentials
+# [profile ac-api-instance-role]
+# credential_process = ABS_PATH/getRoleSession.sh  "ac-api-instance-role" "dev"
 
-# unset previous session
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
-unset AWS_SESSION_TOKEN
+# AWS SDK as well as AWS CLI will handle session expiration/key rotation automatically
+# please make sure to export AWS_PROFILE=role so the service or API can use the profile
 
-#
-#ARG1=${1:-foo}
+# It is also vital, that the script does not return anything else but the given JSON. 
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-sourcing-external.html
+
+ROLE=$1
 PROFILE="${2:-default}"
-echo $PROFILE
-
+DURATION="${3:-3600}"
 
 # fetch the accountid
 identity=$(aws sts get-caller-identity --profile ${PROFILE})
 ACCOUNTID="$(echo $identity | jq '.Account' | sed 's/"//g')"
 
-ROLE=$1
 PROFILENAME="${ROLE}.temp"
-session=$(aws sts assume-role --role-arn arn:aws:iam::${ACCOUNTID}:role/${ROLE} --role-session-name ${PROFILENAME} --profile ${PROFILE})
-
-# DEBUG SESSION
-#echo $session
+session=$(aws sts assume-role --role-arn arn:aws:iam::${ACCOUNTID}:role/${ROLE} --role-session-name ${PROFILENAME} --profile ${PROFILE} --duration ${DURATION})
 
 # set new session
 ACCESSKEY="$(echo $session | jq '.Credentials.AccessKeyId' | sed 's/"//g')"
 SECRETKEY="$(echo $session | jq '.Credentials.SecretAccessKey' | sed 's/"//g')"
 SESSIONTOKEN="$(echo $session | jq '.Credentials.SessionToken' | sed 's/"//g')"
+EXPIRATION="$(echo $session | jq '.Credentials.Expiration' | sed 's/"//g')"
 
-aws configure set aws_access_key_id $ACCESSKEY --profile $PROFILENAME 2> /dev/null
-aws configure set aws_secret_access_key $SECRETKEY --profile $PROFILENAME 2> /dev/null
-aws configure set aws_session_token $SESSIONTOKEN --profile $PROFILENAME 2> /dev/null
-
-echo "AWS Session for is ready and valid for 1 hour. Use --profile ${PROFILENAME}."
+# return JSON according to LINK above
+cat << EOF
+{
+  "Version": 1,
+  "AccessKeyId": "$ACCESSKEY",
+  "SecretAccessKey": "$SECRETKEY",
+  "SessionToken": "$SESSIONTOKEN",
+  "Expiration": "$EXPIRATION"
+}
+EOF
